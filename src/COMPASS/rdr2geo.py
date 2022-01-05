4e1d5724da8ae805f44e6af665deb338a9d4ecc7
Liang Yu <liangjyu@gmail.com>
2022-01-05 15:44:17 -0800

    fixed imports, logging, typos

--- src/COMPASS/rdr2geo.py
+++ src/COMPASS/rdr2geo.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-'''
-wrapper for rdr2geo
-'''
+'''wrapper for rdr2geo'''
 
 from itertools import cycle
 import logging
@@ -11,20 +9,24 @@ import time
 
 import isce3
 
-from runconfig import RunConfig
-from yaml_argparse import YamlArgparse
-
+from sentinel1_reader.sentinel1_reader import burst_from_zip
+from .util.runconfig import RunConfig
+from .util.yaml_argparse import YamlArgparse
 
 def run(cfg):
+    '''run rdr2geo with provided runconfig'''
+    t_start = time.process_time()
+    logging.info("starting rdr2geo")
+
     # find bursts identified in cfg.burst_id
     bursts = []
     pols = cfg.polarization
     i_subswaths = [1, 2, 3]
     zip_list = zip(pols, cycle(i_subswaths)) if len(pols) > 3 else zip(
-        cyle(pols), i_subswaths)
+        cycle(pols), i_subswaths)
     for pol, i_subswath in zip_list:
         temp_bursts = [
-            b for b in zip2bursts(cfg.safe_file, i_subswath, pol)
+            b for b in burst_from_zip(cfg.safe_file, i_subswath, pol)
             if b.burst_id in cfg.burst_id
         ]
         bursts.extend(temp_bursts)
@@ -40,7 +42,7 @@ def run(cfg):
     ellipsoid = proj.ellipsoid
 
     # process rdr2geo for found bursts
-    for i, burst in enumerate(bursts):
+    for burst in bursts:
         # get isce3 objs from burst
         rdr_grid = burst.as_isce3_radargrid()
         isce3_orbit = burst.get_isce3_orbit(cfg.orbit_path)
@@ -66,13 +68,17 @@ def run(cfg):
         # run rdr2geo
         rdr2geo_obj.topo(dem_raster, output_path)
 
+    dt = time.process_time() - t_start
+    logging.info(f"rdr2geo successfully ran in {dt:.3f} seconds")
+
 
 if __name__ == "__main__":
-    '''run rdr2geo from command line
-    '''
+    '''run rdr2geo from command line'''
     # load command line args
     rdr2geo_parser = YamlArgparse()
+
     # get a runconfig dict from command line args
-    rdr2geo_runconfig = RunConfig(rdr2geo_parser.args)
+    rdr2geo_runconfig = RunConfig.load_from_yaml(rdr2geo_parser.args, 'rdr2geo')
+
     # run rdr2geo
     run(rdr2geo_runconfig)

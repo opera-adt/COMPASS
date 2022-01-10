9a1350eec01167040cf8f0eb122fa7e23c9a04e0
Liang Yu <liangjyu@gmail.com>
2022-01-10 14:22:22 -0800

    added run_config_path property

--- src/compass/utils/yaml_argparse.py
+++ src/compass/utils/yaml_argparse.py
@@ -8,3 +8,7 @@ class YamlArgparse():
         parser.add_argument('run_config_path', type=str, nargs='?',
                                  default=None, help='Path to run config file')
         self.args = parser.parse_args()
+
+    @property
+    def run_config_path(self) -> str:
+        return self.args.run_config_path

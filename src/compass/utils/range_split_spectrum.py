2b767fddc5304895833328b10f0c07a2360067fb
vbrancat <virginia.brancato@jpl.nasa.gov>
2022-04-13 12:50:46 -0700

    Avoid saving valid part of burst

--- src/compass/utils/range_split_spectrum.py
+++ src/compass/utils/range_split_spectrum.py
@@ -4,63 +4,6 @@ from isce3.splitspectrum import splitspectrum
 from osgeo import gdal
 
 
-def save_valid_burst(burst, lines_per_block,
-                     scratch_path):
-    '''
-    Save locally the valid part of a S1-A/B burst
-    (Done with block processing to avoid memory issues)
-
-    Parameters:
-    ----------
-    burst: Sentinel1BurstSlc
-       S1-A/B burst object
-    lines_per_block: int
-       Number of lines to read/process in batch
-    scratch_path: str
-       Directory where to store temp data
-
-    Returns:
-    -------
-    burst_path: str
-       Filepath to the locally
-    '''
-    valid_length = burst.last_valid_line - burst.first_valid_line
-    valid_width = burst.last_valid_sample - burst.first_valid_sample
-
-    invalid_burst_path = f'{scratch_path}/invalid_burst_temp'
-    valid_burst_path = invalid_burst_path.replace('invalid', 'valid')
-
-    burst.slc_to_file(invalid_burst_path)
-    # Read invalid burst and create output valid burst
-    in_ds = gdal.Open(invalid_burst_path, gdal.GA_ReadOnly)
-    driver = gdal.GetDriverByName('ENVI')
-    out_ds = driver.Create(valid_burst_path, valid_width, valid_length,
-                           in_ds.RasterCount, gdal.GDT_CFloat32)
-
-    # Start block processing
-    lines_per_block = min(valid_length, lines_per_block)
-    num_blocks = int(np.ceil(valid_length / lines_per_block))
-
-    for band in range(in_ds.RasterCount):
-        for block in range(num_blocks):
-            line_start = block * lines_per_block
-            if block == num_blocks - 1:
-                block_length = valid_length - line_start
-            else:
-                block_length = lines_per_block
-
-            # Read a block of burst data
-            burst_data = in_ds.GetRasterBand(band+1).ReadAsArray(
-                burst.first_valid_sample,
-                burst.first_valid_line + line_start,
-                valid_width, block_length)
-
-            # Write extracted data
-            out_ds.GetRasterBand(band+1).WriteArray(burst_data, yoff=line_start)
-    out_ds.FlushCache()
-    return valid_burst_path
-
-
 def range_split_spectrum(burst, cfg_split_spectrum,
                          scratch_path):
     '''
@@ -110,16 +53,16 @@ def range_split_spectrum(burst, cfg_split_spectrum,
         slant_range=rdr_grid.slant_range,
         freq='A')
 
-    # Save locally the valid part of the burst. Note, invalid data (zeros)
-    # cause issue when computing fft when splitting the range bandwidth
-    valid_burst_path = save_valid_burst(burst, lines_per_block, scratch_path)
+    # Save the burst locally
+    burst_path = f'{scratch_path}/burst_temp.vrt'
+    burst.slc_to_vrt_file(burst_path)
 
-    # Open valid burst and create output burst. The output burst will
+    # The output burst will
     # contain 3 bands: Band #1: low-band image; Band #2 main-band image;
     # Band #3: high-band image.
-    in_ds = gdal.Open(valid_burst_path, gdal.GA_ReadOnly)
-    valid_length = in_ds.RasterYSize
-    valid_width = in_ds.RasterXSize
+    valid_length = burst.last_valid_line - burst.first_valid_line
+    valid_width = burst.last_valid_sample - burst.first_valid_sample
+    in_ds = gdal.Open(burst_path, gdal.GA_ReadOnly)
     driver = gdal.GetDriverByName('ENVI')
     out_ds = driver.Create(f'{scratch_path}/{burst.burst_id}_low_main_high',
                            width, length, 3, gdal.GDT_CFloat32)
@@ -136,7 +79,8 @@ def range_split_spectrum(burst, cfg_split_spectrum,
             block_length = lines_per_block
 
         # Read a block of valid burst data
-        burst_data = in_ds.GetRasterBand(1).ReadAsArray(0, line_start,
+        burst_data = in_ds.GetRasterBand(1).ReadAsArray(burst.first_valid_sample,
+                                                        burst.first_valid_line + line_start,
                                                         valid_width, block_length)
         # Get the low band sub-image and corresponding metadata
         burst_low_data, burst_low_meta = split_spectrum_params.bandpass_shift_spectrum(

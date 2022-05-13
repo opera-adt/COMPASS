e0be9797db6ed14844b1531983a8fe3275a3d342
Liang Yu <liangjyu@gmail.com>
2022-05-13 15:07:57 -0700

    refactored yaml_argparse to accommodate new arguments (#1)
    * refactored yaml_argparse to accommodate new arguments
    refactored s1_cslc to allow for grid type

--- src/compass/s1_cslc.py
+++ src/compass/s1_cslc.py
@@ -1,5 +1,3 @@
-from ruamel.yaml import YAML
-
 from compass import s1_rdr2geo, s1_geo2rdr, s1_resample, s1_geocode_slc
 from compass.utils.geo_runconfig import GeoRunConfig
 from compass.utils.runconfig import RunConfig
@@ -7,19 +5,9 @@ from compass.utils.yaml_argparse import YamlArgparse
 
 
 def main():
-    # load command line args
-    parser = YamlArgparse()
+    parser = YamlArgparse(add_grid_type=True)
 
-    # read yaml file
-    yaml = YAML(typ='safe')
-    with open(parser.run_config_path, 'r') as f:
-        yaml_dict = yaml.load(f)
-
-    proc_steps = set(yaml_dict['runconfig']['groups']['processing'].keys())
-    proc_steps_radar = set(['rdr2geo', 'geo2rdr', 'resample'])
-    proc_steps_geo = set(['geocoding'])
-
-    if proc_steps_radar.issubset(proc_steps):
+    if parser.args.grid_type == 'radar':
         # CSLC workflow in radar coordinates
         # get a runconfig dict from command line args
         cfg = RunConfig.load_from_yaml(parser.run_config_path, 's1_cslc_radar')
@@ -33,16 +21,13 @@ def main():
             s1_geo2rdr.run(cfg)
             s1_resample.run(cfg)
 
-    elif proc_steps_geo.issubset(proc_steps):
+    elif parser.args.grid_type == 'geo':
         # CSLC workflow in geo-coordinates
         # get a runconfig dict from command line argumens
         cfg = GeoRunConfig.load_from_yaml(parser.run_config_path, 's1_cslc_geo')
 
         # run geocode_slc
         s1_geocode_slc.run(cfg)
-
-    else:
-        raise ValueError(f'un-recognized processing steps in the yaml file: {proc_steps}!')
 
 
 if __name__ == "__main__":

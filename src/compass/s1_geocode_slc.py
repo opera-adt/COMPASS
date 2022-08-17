8f9581be6ed9a185a0d0e4b38245c7506e99f0f5
vbrancat <virginia.brancato@jpl.nasa.gov>
2022-08-17 10:05:34 -0700

    Add rdr2geo computation to geocoded SLC workflow

--- src/compass/s1_geocode_slc.py
+++ src/compass/s1_geocode_slc.py
@@ -11,6 +11,7 @@ import journal
 import numpy as np
 from osgeo import gdal
 
+from compass import s1_rdr2geo
 from compass.utils.geo_metadata import GeoCslcMetadata
 from compass.utils.geo_runconfig import GeoRunConfig
 from compass.utils.helpers import get_module_name
@@ -65,6 +66,9 @@ def run(cfg):
 
     # Get azimuth polynomial coefficients for this burst
     az_carrier_poly2d = burst.get_az_carrier_poly()
+
+    # Generate required topo layers
+    s1_rdr2geo.run(cfg)
 
     # Split the range bandwidth of the burst, if required
     if cfg.split_spectrum_params.enabled:

2c3bbc9c42301b12259758712d405cdb185d303c
vbrancat <virginia.brancato@jpl.nasa.gov>
2022-08-18 10:02:28 -0700

    Include generation of topo layers in geocode CSLC

--- src/compass/s1_geocode_slc.py
+++ src/compass/s1_geocode_slc.py
@@ -12,6 +12,7 @@ import numpy as np
 from osgeo import gdal
 
 from compass import s1_rdr2geo
+from compass import s1_geocode_topo
 from compass.utils.geo_metadata import GeoCslcMetadata
 from compass.utils.geo_runconfig import GeoRunConfig
 from compass.utils.helpers import get_module_name
@@ -69,6 +70,8 @@ def run(cfg):
 
     # Generate required topo layers
     s1_rdr2geo.run(cfg, save_in_scratch=True)
+    if cfg.rdr2geo_params.geocode_topo_layers:
+        s1_geocode_topo.run(cfg, fetch_from_scratch=True)
 
     # Split the range bandwidth of the burst, if required
     if cfg.split_spectrum_params.enabled:

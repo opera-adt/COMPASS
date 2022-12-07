cbff3c2f95f7543ff6ff50131608f221bd592729
Seongsu Jeong <sjeong.kr@gmail.com>
2022-12-07 15:57:23 -0800

    Update src/compass/utils/lut.py
    Co-authored-by: Heresh Fattahi <hersh.fattahi@gmail.com>

--- src/compass/utils/lut.py
+++ src/compass/utils/lut.py
@@ -34,7 +34,7 @@ def compute_geocoding_correction_luts(burst, rg_step=200, az_step=0.25, dem_path
     '''
 
     az_bistatic = burst.bistatic_delay(range_step=rg_step, az_step=az_step)
-    rg_doppler = burst.geometrical_and_steering_doppler(range_step=rg_step, az_step=az_step)
+    rg_lut = burst.geometrical_and_steering_doppler(range_step=rg_step, az_step=az_step)
 
     if dem_path is None:
         raise ValueError('DEM for azimith FM rate mismatch was not provided.')

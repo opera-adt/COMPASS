2640ccedea20c3a4c4da3fec001720d41b58426e
Seongsu Jeong <sjeong.kr@gmail.com>
2022-12-07 15:09:23 -0800

    docstring fixed; variable name changed

--- src/compass/utils/lut.py
+++ src/compass/utils/lut.py
@@ -1,4 +1,4 @@
-def compute_geocoding_correction_luts(burst, rg_step=200, az_step=0.25, path_dem=None, path_scratch=None):
+def compute_geocoding_correction_luts(burst, rg_step=200, az_step=0.25, dem_path=None, scratch_path=None):
     '''
     Compute slant range and azimuth LUTs corrections
     to be applied during burst geocoding
@@ -8,13 +8,13 @@ def compute_geocoding_correction_luts(burst, rg_step=200, az_step=0.25, path_dem
     burst: Sentinel1BurstSlc
         S1-A/B burst object
     xstep: int
-        LUT spacing (in pixels) along x/slant range in meters
+        LUT spacing along x/slant range in meters
     ystep: int
-        LUT spacing (in pixels) along x/azimuth in seconds
-    path_dem: str
+        LUT spacing along y/azimuth in seconds
+    dem_path: str
         Path to the DEM required for azimuth FM rate mismatch.
         If `None`, the calculation of the FM mismatch will be skipped.
-    path_scratch: str
+    scratch_path: str
         Path to the scratch directory.
         If `None`, `burst.az_fm_rate_mismatch_mitigation()` will
         create temporary directory internally.
@@ -30,12 +30,12 @@ def compute_geocoding_correction_luts(burst, rg_step=200, az_step=0.25, path_dem
     az_bistatic = burst.bistatic_delay(range_step=rg_step, az_step=az_step)
     rg_doppler = burst.geometrical_and_steering_doppler(range_step=rg_step, az_step=az_step)
 
-    if path_dem is None:
+    if dem_path is None:
         # Skip the FM mismatch rate calculation
         az_fm_mismatch = None
     else:
-        az_fm_mismatch = burst.az_fm_rate_mismatch_mitigation(path_dem,
-                                                              path_scratch,
+        az_fm_mismatch = burst.az_fm_rate_mismatch_mitigation(dem_path,
+                                                              scratch_path,
                                                               rg_step=rg_step,
                                                               az_step=az_step)
 

047ef11408cec672178e823d0bb7db6fef95b06b
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2022-12-19 20:34:10 -0800

    Separate bistatic and azimuth FM rate

--- src/compass/utils/lut.py
+++ src/compass/utils/lut.py
@@ -4,8 +4,9 @@ Placeholder for model-based correction LUT
 import os
 import isce3
 
-def compute_geocoding_correction_luts(burst, rg_step=200, az_step=0.25,
-                                      dem_path, scratch_path=None):
+def compute_geocoding_correction_luts(burst, dem_path=None,
+                                      rg_step=200, az_step=0.25,
+                                      scratch_path=None):
     '''
     Compute slant range and azimuth LUTs corrections
     to be applied during burst geocoding
@@ -14,13 +15,13 @@ def compute_geocoding_correction_luts(burst, rg_step=200, az_step=0.25,
     ----------
     burst: Sentinel1BurstSlc
         S1-A/B burst object
+    dem_path: str
+        Path to the DEM required for azimuth FM rate mismatch.
     xstep: int
         LUT spacing along x/slant range in meters
     ystep: int
         LUT spacing along y/azimuth in seconds
-    dem_path: str
-        Path to the DEM required for azimuth FM rate mismatch.
-        If `None`, the function will raise an error.
+    
     scratch_path: str
         Path to the scratch directory.
         If `None`, `burst.az_fm_rate_mismatch_mitigation()` will
@@ -52,9 +53,6 @@ def compute_geocoding_correction_luts(burst, rg_step=200, az_step=0.25,
     bistatic_delay = burst.bistatic_delay(range_step=rg_step, az_step=az_step)
     geometrical_steering_doppler= burst.geometrical_and_steering_doppler(range_step=rg_step,
                                                                          az_step=az_step)
-
-    if dem_path is None:
-        raise ValueError('DEM for azimuth FM rate mismatch was not provided.')
 
     if not os.path.exists(dem_path):
         raise FileNotFoundError(f'Cannot find the dem file: {dem_path}')

4defe3de7402455087643c0a9b6f613b288277e9
vbrancat <virginia.brancato@jpl.nasa.gov>
2023-03-14 08:29:01 -0700

    Rename product_path to product_group

--- src/compass/utils/geo_runconfig.py
+++ src/compass/utils/geo_runconfig.py
@@ -93,7 +93,7 @@ class GeoRunConfig(RunConfig):
                    user_plus_default_yaml_str, output_paths, geogrids)
 
     @property
-    def product_path(self) -> dict:
+    def product_group(self) -> dict:
         return self.groups.product_path_group
 
     @property

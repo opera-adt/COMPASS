4415559b67ce91e66e9bf1c94b8c88a692d7a4cd
Liang Yu <liangjyu@gmail.com>
2023-03-21 13:01:18 -0700

    write x/y coords/spacings to topo.h5

--- src/compass/s1_geocode_metadata.py
+++ src/compass/s1_geocode_metadata.py
@@ -3,7 +3,6 @@
 '''wrapper to geocode metadata layers'''
 
 from datetime import timedelta
-import os
 import time
 
 import h5py
@@ -11,9 +10,10 @@ import isce3
 import journal
 import numpy as np
 
-from osgeo import gdal
-from compass.utils.runconfig import RunConfig
-from compass.utils.helpers import get_module_name
+from compass import s1_rdr2geo
+from compass.utils.h5_helpers import init_geocoded_dataset
+from compass.utils.helpers import bursts_grouping_generator, get_module_name
+from compass.utils.geo_runconfig import GeoRunConfig
 from compass.utils.yaml_argparse import YamlArgparse
 
 
@@ -23,8 +23,10 @@ def run(cfg, burst, fetch_from_scratch=False):
 
     Parameters
     ----------
-    cfg: dict
-        Dictionary with user runconfig options
+    cfg: GeoRunConfig
+        GeoRunConfig object with user runconfig options
+    burst: Sentinel1BurstSlc
+        Object containing burst parameters needed for geocoding
     fetch_from_scratch: bool
         If True grabs metadata layers from scratch dir
     '''
@@ -44,7 +46,6 @@ def run(cfg, burst, fetch_from_scratch=False):
     threshold = cfg.geo2rdr_params.threshold
     iters = cfg.geo2rdr_params.numiter
     lines_per_block = cfg.geo2rdr_params.lines_per_block
-    output_format = cfg.geocoding_params.output_format
 
     # process one burst only
     date_str = burst.sensing_start.strftime("%Y%m%d")
@@ -64,15 +65,15 @@ def run(cfg, burst, fetch_from_scratch=False):
         input_path = out_paths.scratch_directory
 
     # Initialize geocode object
-    geo = isce3.geocode.GeocodeFloat32()
-    geo.orbit = orbit
-    geo.ellipsoid = ellipsoid
-    geo.doppler = image_grid_doppler
-    geo.threshold_geo2rdr = threshold
-    geo.numiter_geo2rdr = iters
+    geocode_obj = isce3.geocode.GeocodeFloat32()
+    geocode_obj.orbit = orbit
+    geocode_obj.ellipsoid = ellipsoid
+    geocode_obj.doppler = image_grid_doppler
+    geocode_obj.threshold_geo2rdr = threshold
+    geocode_obj.numiter_geo2rdr = iters
     float_bytes = 4
     block_size = lines_per_block * geo_grid.width * float_bytes
-    geo.geogrid(geo_grid.start_x, geo_grid.start_y,
+    geocode_obj.geogrid(geo_grid.start_x, geo_grid.start_y,
                 geo_grid.spacing_x, geo_grid.spacing_y,
                 geo_grid.width, geo_grid.length, geo_grid.epsg)
 
@@ -91,30 +92,36 @@ def run(cfg, burst, fetch_from_scratch=False):
                    }
 
     out_h5 = f'{out_paths.output_directory}/topo.h5'
-    shape = (geo_grid.length, geo_grid.width)
-    with h5py.File(out_h5, 'w') as topo_h5:
+    with h5py.File(out_h5, 'w') as topo_root_h5:
+        # Geocode designated layers
         for layer_name, enabled in meta_layers.items():
             if not enabled:
                 continue
+
             dtype = np.single
             # layoverShadowMask is last option, no need to change data type
             # and interpolator afterwards
             if layer_name == 'layover_shadow_mask':
-                geo.data_interpolator = 'NEAREST'
+                geocode_obj.data_interpolator = 'NEAREST'
                 dtype = np.byte
 
-            topo_ds = topo_h5.create_dataset(layer_name, dtype=dtype,
-                                             shape=shape)
-            topo_ds.attrs['description'] = np.string_(layer_name)
+            # Create dataset with x/y coords/spacing and projection
+            topo_ds = init_geocoded_dataset(topo_root_h5, layer_name, geo_grid,
+                                            dtype, np.string_(layer_name))
+
+            # Init output and input isce3.io.Raster objects for geocoding
             output_raster = isce3.io.Raster(f"IH5:::ID={topo_ds.id.id}".encode("utf-8"),
                                             update=True)
 
             input_raster = isce3.io.Raster(f'{input_path}/{layer_name}.rdr')
 
-            geo.geocode(radar_grid=radar_grid, input_raster=input_raster,
-                        output_raster=output_raster, dem_raster=dem_raster,
-                        output_mode=isce3.geocode.GeocodeOutputMode.INTERP,
-                        min_block_size=block_size, max_block_size=block_size)
+            geocode_obj.geocode(radar_grid=radar_grid,
+                                input_raster=input_raster,
+                                output_raster=output_raster,
+                                dem_raster=dem_raster,
+                                output_mode=isce3.geocode.GeocodeOutputMode.INTERP,
+                                min_block_size=block_size,
+                                max_block_size=block_size)
             output_raster.set_geotransform(geotransform)
             output_raster.set_epsg(output_epsg)
             del input_raster
@@ -130,7 +137,16 @@ if __name__ == "__main__":
     parser = YamlArgparse()
 
     # Get a runconfig dict from command line args
-    cfg = RunConfig.load_from_yaml(parser.args.run_config_path,
-                                   workflow_name='s1_cslc_radar')
-    # run geocode metadata layers
-    run(cfg)
+    cfg = GeoRunConfig.load_from_yaml(parser.args.run_config_path,
+                                      workflow_name='s1_cslc_geo')
+
+    for _, bursts in bursts_grouping_generator(cfg.bursts):
+        burst = bursts[0]
+
+        # Generate required static layers
+        if cfg.rdr2geo_params.enabled:
+            s1_rdr2geo.run(cfg, save_in_scratch=True)
+
+            # Geocode static layers if needed
+            if cfg.rdr2geo_params.geocode_metadata_layers:
+                run(cfg, burst, fetch_from_scratch=True)

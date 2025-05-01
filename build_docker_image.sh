b3afdb58b3dc83a4557b9b0e56613f1bce62b0a1
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2025-05-01 21:24:38 +0000

    add argument to `build_docker_image.sh` to optionally provide base image as an argument; Update Dockerfile accordingly

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -4,6 +4,15 @@ REPO=opera
 IMAGE=cslc_s1
 TAG=final_0.5.6
 
+docker_build_args="--rm --force-rm --network host -t $REPO/$IMAGE:$TAG -f docker/Dockerfile"
+
+if [ $# -eq 0 ]; then
+    echo "Base image was not specified. Using the default image specified in the Dockerfile."
+else
+    echo "Using $1 as the base image."
+    docker_build_args+=" --build-arg BASE_IMAGE=$1 "
+fi
+
 echo "IMAGE is $REPO/$IMAGE:$TAG"
 
 rm docker/dockerimg_cslc_s1_${TAG}.tar
@@ -12,6 +21,6 @@ rm docker/dockerimg_cslc_s1_${TAG}.tar
 set -ex
 
 #docker build --rm --force-rm --network host -t $REPO/$IMAGE:$TAG -f docker/Dockerfile.isce3_builder .
-docker build --rm --force-rm --network host -t $REPO/$IMAGE:$TAG -f docker/Dockerfile .
+docker build $docker_build_args .
 
 docker save $REPO/$IMAGE:$TAG > docker/dockerimg_cslc_s1_${TAG}.tar
